8da77cce25b7ce5c11624389d557f01abedb1fb1
tlsehdrms124 <tlsehdrms124@naver.com>
2024-01-16 16:50:39 +0900

    feat: JWT 토큰 발급 및 Access 토큰 파싱 기능 개발 (Service단)
    - JWT 토큰을 발급하는 기능을 개발하였습니다 (Service단)
    - Access 토큰을 파싱하는 기능을 개발하였습니다. (Service단)

---
+++ BackEnd/src/main/java/com/ssafy/backend/global/component/jwt/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.backend.global.component.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+    private int accessTokenExpiresIn;
+}
---
+++ BackEnd/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtServiceImpl.java
@@ -0,0 +1,55 @@
+package com.ssafy.backend.global.component.jwt.service;
+
+import com.ssafy.backend.global.component.jwt.JwtIssuer;
+import com.ssafy.backend.global.component.jwt.JwtParser;
+import com.ssafy.backend.global.component.jwt.JwtUtils;
+import com.ssafy.backend.global.component.jwt.dto.TokenDto;
+import com.ssafy.backend.global.component.jwt.dto.TokenMemberInfoDto;
+import io.jsonwebtoken.Claims;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import static com.ssafy.backend.global.component.jwt.JwtUtils.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private final JwtUtils jwtUtils;
+    private final JwtIssuer jwtIssuer;
+    private final JwtParser jwtParser;
+
+
+    @Override
+    public TokenDto issueToken(@NonNull TokenMemberInfoDto tokenMemberInfoDto) {
+        String accessToken = jwtIssuer.issueToken(
+                tokenMemberInfoDto.toClaims(jwtUtils.getAccessTokenExpiredMin()), jwtUtils.getEncodedAccessKey()
+        );
+
+        // refresh 토큰은 redis에 저장
+        // 해당 로직 구현 해야함
+
+        return TokenDto.builder()
+                .accessToken(accessToken)
+                .accessTokenExpiresIn(jwtUtils.getAccessTokenExpiredMin())
+                .grantType(BEARER_PREFIX)
+                .build();
+    }
+
+    @Override
+    public TokenMemberInfoDto parseAccessToken(@NonNull String accessToken) {
+        Claims claims = jwtParser.parseToken(accessToken, jwtUtils.getEncodedAccessKey());
+        if (claims == null) {
+            return null;
+        }
+        return TokenMemberInfoDto.from(claims);
+    }
+
+    @Override
+    public TokenDto reissueToken(@NonNull String memberEmail) {
+        return null;
+    }
+}
